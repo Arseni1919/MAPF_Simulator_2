92e1045990f7c5cafdda5e73af780046f3b1867e
perchik_air <1919ars@gmail.com>
2022-10-09 19:19:50 +0300

    more

--- threading_drafts.py
+++ threading_drafts.py
@@ -3,6 +3,9 @@ import threading
 import time
 
 
+import concurrent.futures
+
+
 def thread_function(name):
     logging.info("Thread %s: starting", name)
     time.sleep(2)
@@ -14,10 +17,5 @@ if __name__ == "__main__":
     logging.basicConfig(format=format, level=logging.INFO,
                         datefmt="%H:%M:%S")
 
-    logging.info("Main    : before creating thread")
-    x = threading.Thread(target=thread_function, args=(1,), daemon=True)
-    logging.info("Main    : before running thread")
-    x.start()
-    logging.info("Main    : wait for the thread to finish")
-    # x.join()
-    logging.info("Main    : all done")
+    with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
+        executor.map(thread_function, range(3))
