8195efa8983e1d2139002749d547f7847858464b
perchik_air <1919ars@gmail.com>
2022-10-10 10:49:09 +0300

    more

--- threading_drafts.py
+++ threading_drafts.py
@@ -1,21 +1,111 @@
 import logging
 import threading
 import time
-
-
 import concurrent.futures
+import random
+import queue
 
 
-def thread_function(name):
-    logging.info("Thread %s: starting", name)
-    time.sleep(2)
-    logging.info("Thread %s: finishing", name)
+class Pipeline(queue.Queue):
+    def __init__(self):
+        super().__init__(maxsize=10)
+
+    def get_message(self, name):
+        logging.debug("%s:about to get from queue", name)
+        value = self.get()
+        logging.debug("%s:got %d from queue", name, value)
+        return value
+
+    def set_message(self, value, name):
+        logging.debug("%s:about to add %d to queue", name, value)
+        self.put(value)
+        logging.debug("%s:added %d to queue", name, value)
+
+
+def consumer(pipeline, event):
+    """Pretend we're saving a number in the database."""
+    while not event.is_set() or not pipeline.empty():
+        message = pipeline.get_message("Consumer")
+        logging.info(
+            "Consumer storing message: %s  (queue size=%s)",
+            message,
+            pipeline.qsize(),
+        )
+
+    logging.info("Consumer received EXIT event. Exiting")
+
+
+def producer(pipeline, event):
+    """Pretend we're getting a number from the network."""
+    while not event.is_set():
+        message = random.randint(1, 101)
+        logging.info("Producer got message: %s", message)
+        pipeline.set_message(message, "Producer")
+
+    logging.info("Producer received EXIT event. Exiting")
 
 
 if __name__ == "__main__":
     format = "%(asctime)s: %(message)s"
     logging.basicConfig(format=format, level=logging.INFO,
                         datefmt="%H:%M:%S")
+    # logging.getLogger().setLevel(logging.DEBUG)
 
-    with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
-        executor.map(thread_function, range(3))
+    pipeline = Pipeline()
+    event = threading.Event()
+    with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+        executor.submit(producer, pipeline, event)
+        executor.submit(consumer, pipeline, event)
+
+        time.sleep(10)
+        logging.info("Main: about to set event")
+        event.set()
+
+#
+# class FakeDatabase:
+#     def __init__(self):
+#         self.value = 0
+#         self._lock = threading.Lock()
+#
+#     def update(self, name):
+#         logging.info("Thread %s: starting update", name)
+#         logging.debug("Thread %s about to lock", name)
+#         with self._lock:
+#             logging.debug(f"Thread {name} has lock")
+#             local_copy = self.value
+#             local_copy += 1
+#             time.sleep(0.1)
+#             self.value = local_copy
+#             logging.debug("Thread %s about to release lock", name)
+#         logging.debug(f"Thread {name} after release")
+#         logging.info("Thread %s: finishing update", name)
+#
+#
+# if __name__ == "__main__":
+#     format = "%(asctime)s: %(message)s"
+#     logging.basicConfig(format=format, level=logging.INFO,
+#                         datefmt="%H:%M:%S")
+#     # logging.getLogger().setLevel(logging.DEBUG)
+#     logging.getLogger().setLevel(logging.WARNING)
+#     database = FakeDatabase()
+#     logging.info("Testing update. Starting value is %d.", database.value)
+#     with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+#
+#         for index in range(2):
+#             executor.submit(database.update, index)
+#     logging.info("Testing update. Ending value is %d.", database.value)
+
+
+# def thread_function(name):
+#     logging.info("Thread %s: starting", name)
+#     time.sleep(2)
+#     logging.info("Thread %s: finishing", name)
+
+
+# if __name__ == "__main__":
+#     format = "%(asctime)s: %(message)s"
+#     logging.basicConfig(format=format, level=logging.INFO,
+#                         datefmt="%H:%M:%S")
+#
+#     with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
+#         executor.map(thread_function, range(3))
