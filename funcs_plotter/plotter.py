a228ce457cf5e4e87f29882bbbd4df8c03524276
perchik_air <1919ars@gmail.com>
2023-08-09 16:55:50 +0300

    more

--- funcs_plotter/plotter.py
+++ funcs_plotter/plotter.py
@@ -112,7 +112,7 @@ class Plotter:
 
         time_per_alg_limit = f'{kwargs["time_per_alg_limit"]}' if 'time_per_alg_limit' in kwargs else '-'
         # self.fig.tight_layout()
-        title = f'{img_png}, {kwargs["i_run"]+1}/{runs_per_n_agents}, time limit: {time_per_alg_limit * 60:0.2f}sec.'
+        title = f'{img_png}, {kwargs["i_run"]+1}/{runs_per_n_agents}, time limit: {float(time_per_alg_limit) * 60:0.0f} sec.'
         self.fig.suptitle(title, fontsize=16)
         plt.pause(0.1)
         print('big plot ends')
